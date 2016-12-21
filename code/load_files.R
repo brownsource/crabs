00d652fadedbd5f156dd4fc4ce33688e552da654
brownsource <harry@brown-source.com>
2016-12-21 10:42:17 +0800

    initialise

---
+++ code/load_files.R
@@ -0,0 +1,11 @@
+# Open directory
+# Provide range of files to open
+# Loop through files
+# Open file
+# Add column with reading Id
+# Add colume with filename
+# Remove rows with NaN
+# Copy the 15 middle rows
+# Save to the master dataframe
+# End loop
+# Save the dataframe
